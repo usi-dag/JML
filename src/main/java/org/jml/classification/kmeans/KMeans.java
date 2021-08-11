01034806ab830c5a0bb25a9cbb0fe564460558c2
Luca Omini <luca.omini@usi.ch>
2021-08-11 11:51:12 +0200

    scalar kmenas test

--- src/main/java/org/jml/classification/kmeans/KMeans.java
+++ src/main/java/org/jml/classification/kmeans/KMeans.java
@@ -186,24 +186,7 @@ public class KMeans {
             dataset[i] = new double[]{Double.parseDouble(temperature.get(i)), Double.parseDouble(humidity.get(i))};
         }
 
-//        dataset[0] = new double[]{0, 0};
-//        dataset[1] = new double[]{0, 1};
-//        dataset[2] = new double[]{0, 2};
-//        dataset[3] = new double[]{1, 0};
-//        dataset[4] = new double[]{1, 1};
-//        dataset[5] = new double[]{1, 2};
-//        dataset[6] = new double[]{2, 0};
-//        dataset[7] = new double[]{2, 1};
-//        dataset[8] = new double[]{2, 2};
-//        dataset[9] = new double[]{5, 5};
-//        dataset[10] = new double[]{5, 6};
-//        dataset[11] = new double[]{5, 7};
-//        dataset[12] = new double[]{6, 5};
-//        dataset[13] = new double[]{6, 6};
-//        dataset[14] = new double[]{6, 7};
-//        dataset[15] = new double[]{7, 5};
-//        dataset[16] = new double[]{7, 6};
-//        dataset[17] = new double[]{7, 7};
+
 
         kMeans.fit(dataset, 3);
 
