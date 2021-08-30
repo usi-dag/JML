0c430f77378a237ac77f34b83db1b0163beb5635
Luca Omini <luca.omini@usi.ch>
2021-08-30 19:39:35 +0200

    Add a copy of libsvm.svm for debug problem
    problem with floating point rounding error -> to many vectorization brings to have a completly different model
    therefore need to take care of non associative operation in the vectorization

--- src/main/java/org/jml/classification/svm/BenchmarkSvm.java
+++ src/main/java/org/jml/classification/svm/BenchmarkSvm.java
@@ -13,69 +13,69 @@ import java.util.concurrent.TimeUnit;
 
 public class BenchmarkSvm {
 
-    private static final int ITERATIONS = 1;
-    static Random r = new Random();
-
-    static class Data {
-        private double[][] X;
-        private double[] Y;
-
-        Data(int n, int low, int high, double m, double q, int dimension) {
-            linearBinaryClass(n, low, high, m, q, dimension);
-        }
-
-        public double[][] getX() {
-            return X;
-        }
-
-        public double[] getY() {
-            return Y;
-        }
-
-        private void linearBinaryClass(int n, int low, int high, double m, double q, int dimension) {
-            X = new double[n][dimension];
-            Y = new double[n];
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < dimension; j++) {
-                    X[i][j] = r.nextInt(high - low) + low;
-                }
-            }
-
-            for (int i = 0; i < n; i++) {
-                if (X[i][1] - (X[i][0] * m + q) > 0) Y[i] = 1;
-                else Y[i] = -1;
-            }
-        }
-    }
-
-
-
-
-    @State(Scope.Benchmark)
-    public static class MyState {
-        Data data = new Data(200, 0, 20, 0.8, 0, 2);
-        public double[][] X = data.getX();
-        public double[] Y = data.getY();
-    }
-
-    @Benchmark
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    public void testSvm(MyState state, Blackhole sink) {
-        Svm svm = new Svm();
-        for (int i = 0; i < ITERATIONS; i++) {
-            svm.fit(state.X, state.Y);
-            sink.consume(svm.predict(new double[]{3, 0.5}));
-        }
-    }
-
-    @Benchmark
-    @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    public void testKSvmVectorized(MyState state, Blackhole sink) {
-        Svm svm = new Svm(true);
-        for (int i = 0; i < ITERATIONS; i++) {
-            svm.fit(state.X, state.Y);
-            sink.consume(svm.predict(new double[]{3, 0.5}));
-        }
-    }
+//    private static final int ITERATIONS = 1;
+//    static Random r = new Random();
+//
+//    static class Data {
+//        private double[][] X;
+//        private double[] Y;
+//
+//        Data(int n, int low, int high, double m, double q, int dimension) {
+//            linearBinaryClass(n, low, high, m, q, dimension);
+//        }
+//
+//        public double[][] getX() {
+//            return X;
+//        }
+//
+//        public double[] getY() {
+//            return Y;
+//        }
+//
+//        private void linearBinaryClass(int n, int low, int high, double m, double q, int dimension) {
+//            X = new double[n][dimension];
+//            Y = new double[n];
+//
+//            for (int i = 0; i < n; i++) {
+//                for (int j = 0; j < dimension; j++) {
+//                    X[i][j] = r.nextInt(high - low) + low;
+//                }
+//            }
+//
+//            for (int i = 0; i < n; i++) {
+//                if (X[i][1] - (X[i][0] * m + q) > 0) Y[i] = 1;
+//                else Y[i] = -1;
+//            }
+//        }
+//    }
+//
+//
+//
+//
+//    @State(Scope.Benchmark)
+//    public static class MyState {
+//        Data data = new Data(200, 0, 20, 0.8, 0, 2);
+//        public double[][] X = data.getX();
+//        public double[] Y = data.getY();
+//    }
+//
+//    @Benchmark
+//    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+//    public void testSvm(MyState state, Blackhole sink) {
+//        Svm svm = new Svm();
+//        for (int i = 0; i < ITERATIONS; i++) {
+//            svm.fit(state.X, state.Y);
+//            sink.consume(svm.predict(new double[]{3, 0.5}));
+//        }
+//    }
+//
+//    @Benchmark
+//    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+//    public void testKSvmVectorized(MyState state, Blackhole sink) {
+//        Svm svm = new Svm(true);
+//        for (int i = 0; i < ITERATIONS; i++) {
+//            svm.fit(state.X, state.Y);
+//            sink.consume(svm.predict(new double[]{3, 0.5}));
+//        }
+//    }
 }
---
+++ src/main/java/org/jml/libsvm/SvmBenchmark.java
@@ -0,0 +1,168 @@
+package org.jml.libsvm;
+
+
+import libsvm.*;
+import org.jml.dataset.LoadCSV;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class SvmBenchmark {
+
+    private static final int ITERATIONS = 1;
+
+    static class Data {
+        private svm_node[][] X;
+        private double[] Y;
+        private static svm_parameter param = new svm_parameter();
+        private static svm_problem prob = new svm_problem();
+
+        Data() {
+            param.svm_type = svm_parameter.C_SVC;
+            param.kernel_type = svm_parameter.LINEAR;
+            param.degree = 3;
+            param.gamma = 0;	// 1/num_features
+            param.coef0 = 0;
+            param.nu = 0.5;
+            param.cache_size = 100;
+            param.C = 1;
+            param.eps = 1e-3;
+            param.p = 0.1;
+            param.shrinking = 1;
+            param.probability = 0;
+            param.nr_weight = 0;
+            param.weight_label = new int[0];
+            param.weight = new double[0];
+
+            LoadCSV loader = new LoadCSV("winequality-red.csv");
+
+            Map<String, List<String>> records = loader.getRecords();
+
+            List<String> acidity = records.get("fixed acidity");
+            List<String> volatile_acidity = records.get("volatile acidity");
+            List<String> citric_acidity = records.get("citric acid");
+            List<String> residual_sugar = records.get("residual sugar");
+            List<String> chlorides = records.get("chlorides");
+            List<String> free_sulfur_dioxide = records.get("free sulfur dioxide");
+            List<String> total_sulfur_dioxide = records.get("total sulfur dioxide");
+            List<String> density = records.get("density");
+            List<String> pH = records.get("pH");
+            List<String> sulphates = records.get("sulphates");
+            List<String> alcohol = records.get("alcohol");
+            List<String> quality = records.get("quality");
+
+            int size = acidity.size();
+            X = new svm_node[size][11];
+
+            List<List<String>> tmp = new ArrayList<>();
+            tmp.add(acidity);
+            tmp.add(volatile_acidity);
+            tmp.add(citric_acidity);
+            tmp.add(residual_sugar);
+            tmp.add(chlorides);
+            tmp.add(free_sulfur_dioxide);
+            tmp.add(total_sulfur_dioxide);
+            tmp.add(density);
+            tmp.add(pH);
+            tmp.add(sulphates);
+            tmp.add(alcohol);
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < 11; j++) {
+                    X[i][j] = new svm_node();
+                    X[i][j].index = j;
+                    X[i][j].value = Double.parseDouble(tmp.get(j).get(i));
+                }
+            }
+
+            prob.l = size;
+            prob.x = X;
+            prob.y = quality.stream().mapToDouble(Double::parseDouble).toArray();
+
+            if(param.gamma == 0 && size > 0)
+                param.gamma = 1.0/size;
+        }
+
+        public static svm_problem getProb() {
+            return prob;
+        }
+
+        public static svm_parameter getParam() {
+            return param;
+        }
+
+        public svm_node[][] getX() {
+            return X;
+        }
+
+        public double[] getY() {
+            return Y;
+        }
+    }
+
+
+
+
+    @State(Scope.Benchmark)
+    public static class MyState {
+        Data data = new Data();
+        public svm_parameter param = data.getParam();
+        public svm_problem prob = data.getProb();
+
+        public svm_node[] pred = createPred();
+
+        svm_node[] createPred() {
+            svm_node[] x = new svm_node[11];
+            for (int i = 0; i < 11; i++) {
+                x[i] = new svm_node();
+                x[i].index = i;
+                x[i].value = i - 0.5;
+            }
+
+            return x;
+        }
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void testLibSvm(MyState state, Blackhole sink) {
+        svm svm_class = new svm();
+        svm_model model;
+        for (int i = 0; i < ITERATIONS; i++) {
+            model = svm_class.svm_train(state.prob, state.param);
+            System.out.println(svm_class.svm_predict(model, state.pred));
+            sink.consume(svm_class.svm_predict(model, state.pred));
+        }
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void testLibSvmVector(MyState state, Blackhole sink) {
+        svm svm_class = new svm(true, true);
+        svm_model model;
+        for (int i = 0; i < ITERATIONS; i++) {
+            model = svm_class.svm_train(state.prob, state.param);
+            System.out.println(svm_class.svm_predict(model, state.pred));
+            sink.consume(svm_class.svm_predict(model,  state.pred));
+        }
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void testLibSvmVectorNoMask(MyState state, Blackhole sink) {
+        svm svm_class = new svm(true, false);
+        svm_model model;
+        for (int i = 0; i < ITERATIONS; i++) {
+            model = svm_class.svm_train(state.prob, state.param);
+            System.out.println(svm_class.svm_predict(model, state.pred));
+            sink.consume(svm_class.svm_predict(model, state.pred));
+        }
+    }
+}
